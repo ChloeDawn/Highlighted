5ca2b3cd85a8b1025690b5144d8d7f93a326c90f
InsomniaKitten <admin@designdevils.co.uk>
2018-01-29 18:30:47 +0000

    Add render modes (lines are broken, heck)

--- src/main/java/net/insomniakitten/highlighted/Highlighted.java
+++ src/main/java/net/insomniakitten/highlighted/Highlighted.java
@@ -1,22 +1,26 @@
 package net.insomniakitten.highlighted;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.GlStateManager.DestFactor;
-import net.minecraft.client.renderer.GlStateManager.SourceFactor;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
 import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -45,18 +49,23 @@ public final class Highlighted {
     @SubscribeEvent
     public static void onRenderWorldLast(RenderWorldLastEvent event) {
         Minecraft mc = FMLClientHandler.instance().getClient();
-        if (mc.objectMouseOver == null) return;
-        if (mc.objectMouseOver.typeOfHit != Type.BLOCK) return;
-        renderBoxes(mc.player, mc.objectMouseOver.getBlockPos(), event.getPartialTicks());
+        RayTraceResult result = mc.objectMouseOver;
+        if (result == null) return;
+        if (result.typeOfHit != Type.BLOCK) return;
+        renderBoxes(mc.player, result.getBlockPos(), result.sideHit, event.getPartialTicks());
     }
 
-    private static void renderBoxes(EntityPlayer player, BlockPos pos, float partialTicks) {
-        AxisAlignedBB eBox = player.getEntityBoundingBox().grow(6.0D);
+    @SubscribeEvent
+    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (ID.equals(event.getModID())) ConfigManager.sync(ID, Config.Type.INSTANCE);
+    }
+
+    private static void renderBoxes(EntityPlayer player, BlockPos pos, EnumFacing side, float partialTicks) {
         World world = player.world;
         IBlockState state = world.getBlockState(pos);
         List<AxisAlignedBB> boxes = new ArrayList<>();
 
-        state.addCollisionBoxToList(world, pos, eBox, boxes, player, false);
+        state.addCollisionBoxToList(world, pos, Block.FULL_BLOCK_AABB.offset(pos), boxes, player, false);
         if (boxes.isEmpty()) boxes.add(state.getSelectedBoundingBox(world, pos));
 
         double offsetX = player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTicks;
@@ -65,35 +74,33 @@ public final class Highlighted {
 
         GlStateManager.disableAlpha();
         GlStateManager.enableBlend();
-        GlStateManager.tryBlendFuncSeparate(
-                SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA,
-                SourceFactor.ONE, DestFactor.ZERO
-        );
         GlStateManager.enableCull();
         GlStateManager.disableTexture2D();
         GlStateManager.depthMask(false);
 
+        GlStateManager.tryBlendFuncSeparate(
+                GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+                GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO
+        );
+
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder buffer = tessellator.getBuffer();
 
-        buffer.begin(GL11.GL_TRIANGLE_STRIP, DefaultVertexFormats.POSITION_COLOR);
+        buffer.begin(ModConfig.renderMode.glMode, DefaultVertexFormats.POSITION_COLOR);
         buffer.setTranslation(-offsetX, -offsetY, -offsetZ);
 
-        for (AxisAlignedBB box : boxes) {
-            buildCube(buffer, box.grow(0.002D));
-        }
+        ModConfig.renderMode.builder.accept(buffer, state, world, pos, side, boxes);
 
         buffer.setTranslation(0.0D, 0.0D, 0.0D);
         tessellator.draw();
 
         GlStateManager.depthMask(true);
         GlStateManager.enableTexture2D();
-        GlStateManager.disableCull();
         GlStateManager.disableBlend();
         GlStateManager.enableAlpha();
     }
 
-    private static void buildCube(BufferBuilder buffer, AxisAlignedBB box) {
+    private static void buildCubeLines(BufferBuilder buffer, AxisAlignedBB box) {
         float red = 0.01F * ModConfig.highlightRed;
         float green = 0.01F * ModConfig.highlightGreen;
         float blue = 0.01F * ModConfig.highlightBlue;
@@ -102,36 +109,110 @@ public final class Highlighted {
         double minX = box.minX, minY = box.minY, minZ = box.minZ;
         double maxX = box.maxX, maxY = box.maxY, maxZ = box.maxZ;
 
-        buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(minX, minY, minZ).color(red, green, blue, 0.0F).endVertex();
         buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
         buffer.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
         buffer.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
         buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
         buffer.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
         buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
-        buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(minX, maxY, maxZ).color(red, green, blue, 0.0F).endVertex();
+        buffer.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(maxX, maxY, maxZ).color(red, green, blue, 0.0F).endVertex();
+        buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(maxX, maxY, minZ).color(red, green, blue, 0.0F).endVertex();
+        buffer.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        buffer.pos(maxX, minY, minZ).color(red, green, blue, 0.0F).endVertex();
+    }
+
+    private static void buildFaceOverlay(BufferBuilder buffer, AxisAlignedBB box, EnumFacing face) {
+        float red = 0.01F * ModConfig.highlightRed;
+        float green = 0.01F * ModConfig.highlightGreen;
+        float blue = 0.01F * ModConfig.highlightBlue;
+        float alpha = 0.01F * ModConfig.highlightAlpha;
+
+        double minX = box.minX, minY = box.minY, minZ = box.minZ;
+        double maxX = box.maxX, maxY = box.maxY, maxZ = box.maxZ;
+
+        switch (face) {
+            case DOWN:
+                buffer.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
+                break;
+            case UP:
+                buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+                break;
+            case NORTH:
+                buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+                break;
+            case SOUTH:
+                buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+                break;
+            case WEST:
+                buffer.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
+                break;
+            case EAST:
+                buffer.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+                buffer.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+                break;
+        }
+    }
+
+    private enum RenderMode {
+        lines(GL11.GL_LINE_STRIP, (buffer, state, world, pos, face, boxes) -> {
+            GlStateManager.glLineWidth(2.0F);
+            for (AxisAlignedBB box : boxes) {
+                buildCubeLines(buffer, box.grow(0.002D));
+            }
+        }),
+
+        overlay_all(GL11.GL_QUADS, (buffer, state, world, pos, face, boxes) -> {
+            for (EnumFacing side : EnumFacing.VALUES) {
+                if (state.shouldSideBeRendered(world, pos, side)) {
+                    for (AxisAlignedBB box : boxes) {
+                        buildFaceOverlay(buffer, box.grow(0.002D), side);
+                    }
+                }
+            }
+        }),
+
+        overlay_face(GL11.GL_QUADS, (buffer, state, world, pos, face, boxes) -> {
+            for (AxisAlignedBB box : boxes) {
+                buildFaceOverlay(buffer, box.grow(0.002D), face);
+            }
+        });
+
+        private final int glMode;
+        private final BufferConsumer builder;
+
+        RenderMode(int glMode, BufferConsumer builder) {
+            this.glMode = glMode;
+            this.builder = builder;
+        }
+    }
+
+    private interface BufferConsumer {
+        void accept(BufferBuilder buffer, IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing face, List<AxisAlignedBB> boxes);
     }
 
     @Config(modid = ID, name = ID)
@@ -155,6 +236,10 @@ public final class Highlighted {
         @Config.Comment("The alpha percentage to use for the highlight")
         @Config.RangeInt(min = 0, max = 100)
         public static int highlightAlpha = 20;
+
+        @Config.Name("render_mode")
+        @Config.Comment("How should the highlight be rendered?")
+        public static RenderMode renderMode = RenderMode.overlay_face;
     }
 
 }
